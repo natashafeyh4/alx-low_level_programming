75efceb0332b6c17a7fcb6289744c7020fc8ccdf
natashafeyh4 <natashafeyh44@gmail.com>
2023-08-31 03:09:58 -0700

    0-binary_to_uint.c

---
+++ 0-binary_to_uint.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * binary _to_unit -convert a binary number to unsigned int
+ * @b: string containing the binary number
+ *
+ * Return: the converted number
+ */
+unsigned int binary_to_unit(const char *b)
+{
+	int i;
+	unsigned dev_val =0;
+	if(!b)
+		return(0);
+	for(i =0; b[i]; i++)
+		  {
+        if (b[i] < '0' || b[i] > '1')
+            return (0);
+        dec_val = 2 * dec_val + (b[i] - '0');
+    }
+
+    return (dec_val);
+}
