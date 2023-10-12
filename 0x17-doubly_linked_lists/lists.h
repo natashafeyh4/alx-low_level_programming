d1a7f62242e618e9a68d0d5a7e9d3176b8bfcfd0
natashafeyh4 <natashafeyh44@gmail.com>
2023-10-12 12:41:00 -0700

    1-dlistint_len.c

---
+++ 0x17-doubly_linked_lists/lists.h
@@ -0,0 +1,21 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Define your struct for the doubly linked list node */
+typedef struct dlistint_s
+{
+    int n;
+    struct dlistint_s *prev;
+    struct dlistint_s *next;
+} dlistint_t;
+
+/* Function prototypes for the tasks */
+size_t print_dlistint(const dlistint_t *h);
+size_t dlistint_len(const dlistint_t *h);
+dlistint_t *add_dnodeint(dlistint_t **head, const int n);
+/* Add prototypes for other functions you implement */
+
+#endif /* LISTS_H */
